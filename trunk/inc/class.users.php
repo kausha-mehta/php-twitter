19054686f599aeb1d1a02bbbacee5f0859f1a447
emmensetech <emmensetech@18908e41-564e-0410-bc37-b3a4befdf8c3>
2009-11-07 08:14:56 +0000

    Complete User API

--- trunk/inc/class.users.php
+++ trunk/inc/class.users.php
@@ -25,6 +25,60 @@ class Twitter_Users extends Twitter {
 	}
 	
 	/**
+	 * Retrieve data about a specified user.
+	 *
+	 * @access public
+	 * @since 2.0
+	 * @param string/int $user_id The Twitter screen name or ID of a specified user
+	 * @return object
+	 */
+	public function show_user( $user_id )
+	{
+		if( is_int( $user_id ) )
+			$qs = array( 'user_id' => $user_id );
+		else
+			$qs = array( 'screen_name' => $user_id );
+		$this->api_url = 'http://twitter.com/users/show.' . $this->type . $this->_glue( $qs );
+		return $this->_get( $this->api_url );
+	}
+	
+	/**
+	 * Retrieve data about a specified user.
+	 *
+	 * @access public
+	 * @since 2.0
+	 * @param string/int $user_id The Twitter screen name or ID of a specified user
+	 * @return object
+	 */	
+	public function get_friends( $user_id )
+	{
+		if( is_int( $user_id ) )
+			$qs = array( 'user_id' => $user_id );
+		else
+			$qs = array( 'screen_name' => $user_id );
+		$this->api_url = 'http://twitter.com/statuses/friends.' . $this->type . $this->_glue( $qs );
+		return $this->_get( $this->api_url );
+	}
+	
+	/**
+	 * Retrieve data about a specified user.
+	 *
+	 * @access public
+	 * @since 2.0
+	 * @param string/int $user_id The Twitter screen name or ID of a specified user
+	 * @return object
+	 */	
+	public function get_followers( $user_id )
+	{
+		if( is_int( $user_id ) )
+			$qs = array( 'user_id' => $user_id );
+		else
+			$qs = array( 'screen_name' => $user_id );
+		$this->api_url = 'http://twitter.com/statuses/followers.' . $this->type . $this->_glue( $qs );
+		return $this->_get( $this->api_url );
+	}
+	
+	/**
 	 * Destroys the object
 	 *
 	 * @access public
