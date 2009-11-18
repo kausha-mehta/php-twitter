266243a816996982e05a71f0415fe4c99bf30d75
emmensetech <emmensetech@18908e41-564e-0410-bc37-b3a4befdf8c3>
2009-11-18 20:12:02 +0000

    Now working on Twitter Account API

---
+++ inc/class.account.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Twitter_Account_Name
+ *
+ * @package php-twitter 2.0
+ * @subpackage users
+ * @author Aaron Brazell
+ **/
+
+require_once('../class.twitter.php');
+class Twitter_Account_Name extends Twitter {
+	/**
+	 * Setup Twitter client connection details
+	 *
+	 * Ensure you set a user agent, whether via the constructor or by assigning a value to the property directly.
+	 * Also, if you are not running this from the Eastern timezone, be sure to set your proper timezone.
+	 *
+	 * @access public
+	 * @since 2.0
+	 * @return Twitter_Account_Name
+	 */
+	public function __construct( $username = null, $password = null, $user_agent = null, $headers = null, $timezone = 'America/New_York')
+	{
+		parent::__construct($username, $password, $user_agent, $headers, $timezone);
+	}
+
+	/**
+	 * Destroys the object
+	 *
+	 * @access public
+	 * @since 2.0
+	 * @return null
+	 */
+	public function __destruct() {}
+}
