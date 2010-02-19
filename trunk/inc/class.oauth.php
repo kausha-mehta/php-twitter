993e32038255ead10e84e24170c8b551510ce780
emmensetech <emmensetech@18908e41-564e-0410-bc37-b3a4befdf8c3>
2010-02-19 09:45:53 +0000

    beginning oAuth building

---
+++ trunk/inc/class.oauth.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Twitter_OAuth
+ *
+ * @package php-twitter 2.0
+ * @subpackage users
+ * @author Aaron Brazell
+ **/
+
+require_once('../class.twitter.php');
+class Twitter_OAuth extends Twitter {
+	/**
+	 * Setup Twitter client connection details
+	 *
+	 * Ensure you set a user agent, whether via the constructor or by assigning a value to the property directly.
+	 * Also, if you are not running this from the Eastern timezone, be sure to set your proper timezone.
+	 *
+	 * @access public
+	 * @since 2.0
+	 * @return Twitter_Lists
+	 */
+	public function __construct( $username = null, $password = null, $user_agent = null, $headers = null, $timezone = 'America/New_York')
+	{
+		parent::__construct($username, $password, $user_agent, $headers, $timezone);
+	}
+
+	
+	
+	/**
+	 * Destroys the object
+	 *
+	 * @access public
+	 * @since 2.0
+	 * @return null
+	 */
+	public function __destruct() {}
+}
